6e7e5d84cca63f00e984f365de445d75c5e79c6f
Scobra <79657875+ScobraScope@users.noreply.github.com>
2022-01-10 19:42:24 +0100

    First innit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # universal-spammer
-A Python based spammer that works on almost all apps. It simulates your keyboard input and is very easy to use
+--------
+## Download
+You can download the **windows executable** here: (Spammer.exe)[https://placeholder.com]
+
+The python sourcecode is found here (Main.py)[https://placeholder.com]
+
+## Requirements
+The windows (executable)[https://placeholder.com] has no requirements. It can be used right after download.
+
+If you are using the Python file, make sure to have the following modules installed: **time**, **pynput**.
+
+Install them via `pip install pynput` and `pip install time`.
+
+## How-To
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+from pynput.keyboard import Key, Controller
+
+print("""This is a spammer that works on any platform.
+It simulates keyboard input. Make sure to click into a text field after starting the script.
+All time values are treated as seconds.
+Made by 6A31 - https://github.com/ScobraScope
+
+Please be aware that setting the Delay between messages too low can cause your pc to lag / freeze""")
+
+
+des1 = int(input("How long should the script wait before starting to spam? >>> "))
+des2 = int(input("How manny messages should get send? >>> "))
+des3 = str(input("Message: >>> "))
+des4 = float(input("Delay between messages: "))
+
+
+time.sleep(des1)
+keyboard = Controller()
+def enter():
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+i = 1
+des2 += 1
+while i < (des2):
+    keyboard.type(des3 + " " + str(i))
+    enter()
+    i += 1
+    time.sleep(des4)
